9904988898288cc9c3e62bcc5d5bb711da4e96cd
Artur Gorevyi <108293399+Artur-Gorevyi@users.noreply.github.com>
2023-09-17 14:36:59 +0300

    Dict

---
+++ Numberdict.py
@@ -0,0 +1,20 @@
+numberdict = {
+    "674142935": "Anastasiia Starushko",
+    "674143055": "Alla Yachnik",
+    "674142644": "Alexander Oprya",
+    "674143773": "Alexander Kozharko",
+    "674143051": "Alexander Bilyk",
+    "674142215": "Alexander Gorgola",
+    "674142951": "Alexander Hudyaev",
+    "674143031": "Alexey Shut",
+    "674144051": "Anastasіia Karaman",
+    "674143031": "Alexey Shut",
+    "674146076": "Mariia Sukharevska",
+    "674143284": "Oleg Shpin",
+    "674144051": "Anastasiia Karaman",
+}
+
+
+
+
+
